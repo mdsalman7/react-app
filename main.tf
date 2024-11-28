ffd41a7e9cea702a0a19bd1ed6e12ce157135aa7
Md Salman <101348910+mdsalman7@users.noreply.github.com>
2024-11-28 19:52:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -128,7 +128,22 @@ resource "aws_lb" "my_alb" {
   enable_deletion_protection  = false
   enable_cross_zone_load_balancing = true
   enable_http2                = true
-  idle_timeout_seconds        = 60  # Correct argument syntax
+}
+
+resource "aws_lb_listener" "my_listener" {
+  load_balancer_arn = aws_lb.my_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      status_code = 200
+      message_body = "OK"
+    }
+  }
+
+  idle_timeout_seconds = 60  # Correct placement of the argument
 }
 
 
