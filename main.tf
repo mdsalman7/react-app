13c1abde83b87f47cd465961f051dd7432fb5010
Md Salman <101348910+mdsalman7@users.noreply.github.com>
2024-11-29 17:22:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -151,14 +151,14 @@ resource "aws_security_group" "ecs_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    security_group_id = aws_security_group.alb_sg.id
+    source_security_group_id = aws_security_group.alb_sg.id
   }
 
   ingress {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
-    security_group_id = aws_security_group.alb_sg.id
+    source_security_group_id = aws_security_group.alb_sg.id
   }
 
   egress {
